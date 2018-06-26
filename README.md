f80d53a160c78a04f193a973f711c0fb17b8371a
Patkul <tshah011@gmail.com>
2018-06-25 20:15:01 -0400

    updated news scraper

--- README.md
+++ README.md
@@ -1,2 +1,31 @@
-# NPR_NewsScraper
-NPR News Scraper
+# Eat-Da-Burger! 
+Deployed in Heroku
+
+https://still-thicket-14937.herokuapp.com/burgers
+
+## Overview
+
+
+In this assignment, you'll create a burger logger with MySQL, Node, Express, Handlebars and a homemade ORM (yum!). Be sure to follow the MVC design pattern; use Node and MySQL to query and route data in your app, and Handlebars to generate your HTML.
+
+
+## Dependencies
+
+* express
+
+* express-handlebars
+
+* mongoose
+
+* body-parser
+
+* cheerio
+
+* request
+
+![Image of NPR News APP](https://github.com/Tapesh123/Eat-Da-Burger/blob/master/EatDaBurger.gif)
+
+
+Deployed in Heroku
+
+in process
---
+++ server.js
@@ -0,0 +1,63 @@
+
+// Node Dependencies
+var express = require('express');// web application framework 
+var exphbs = require('express-handlebars');
+var bodyParser = require('body-parser');//This parses the body portion of an incoming HTTP request and makes it easier to extract different parts of the contained information. For example, you can use this to read POST parameters.
+var mongoose = require('mongoose');
+var logger = require('morgan'); // for debugging //An HTTP request logger middleware for node.
+var request = require('request'); // for web-scraping
+var cheerio = require('cheerio'); // for web-scraping
+
+
+// Initialize Express for debugging & body parsing
+var app = express();
+app.use(logger('dev'));
+app.use(bodyParser.urlencoded({
+  extended: false
+}))
+
+// Serve Static Content
+app.use(express.static(process.cwd() + '/public'));
+
+// Express-Handlebars
+app.engine('handlebars', exphbs({defaultLayout: 'main'}));
+app.set('view engine', 'handlebars');
+
+
+// Database Configuration with Mongoose
+// ---------------------------------------------------------------------------------------------------------------
+// Connect to localhost if not a production environment
+var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/nprnews"
+
+
+  mongoose.connect(MONGODB_URI);
+
+var db = mongoose.connection;
+
+// Show any Mongoose errors
+db.on('error', function(err) {
+  console.log('Mongoose Error: ', err);
+});
+
+// Once logged in to the db through mongoose, log a success message
+db.once('openUri()', function() {
+  console.log('Mongoose connection successful.');
+});
+//we require() modules from our routes directory. These modules/files contain code for handling particular sets of related "routes" (URL paths).
+// Import the Comment and Article models
+var Comment = require('./models/Comment.js');
+var Article = require('./models/Article.js');
+// ---------------------------------------------------------------------------------------------------------------
+
+
+
+// Import Routes/Controller
+var router = require('./controllers/controller.js');
+app.use('/', router);
+
+
+// Launch App
+var port = process.env.PORT || 3000;
+app.listen(port, function(){
+  console.log('Running on port: ' + port);
+});
